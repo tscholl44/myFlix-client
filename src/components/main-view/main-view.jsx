4e4f34881b0deee499ab2ef0b73f29734e1720d8
tscholl44 <tscholl4444@gmail.com>
2025-02-11 18:37:08 -0500

    fetch API
    replaced hardcode array with API and configured proptypes for MovieCard

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,47 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Inception",
-      image:
-        "https://m.media-amazon.com/images/I/912AErFSBHL._AC_UF894,1000_QL80_.jpg",
-      director: "Christopher Nolan",
-    },
-    {
-      id: 2,
-      title: "Shawshank Redemption",
-      image:
-        "https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_.jpg",
-      director: "Frank Darabont",
-    },
-    {
-      id: 3,
-      title: "Apocalypto",
-      image:
-        "https://m.media-amazon.com/images/M/MV5BMzJkNzI3ZmEtZmRhOS00YTM1LWI3OTAtZmVjZmNkODIxNmFmXkEyXkFqcGc@._V1_.jpg",
-      director: "Mel Gibson",
-    },
-    {
-      id: 4,
-      title: "Platoon",
-      image:
-        "https://m.media-amazon.com/images/I/61juOMdMMIL._AC_UF894,1000_QL80_.jpg",
-      director: "Oliver Stone",
-    },
-    {
-      id: 5,
-      title: "Shrek",
-      image:
-        "https://images-na.ssl-images-amazon.com/images/I/41MBLi5a4jL._SX384_BO1,204,203,200_.jpg",
-      director: "Andrew Adamson",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
+  
+  useEffect(() => {
+    fetch("https://toms-flix-a1bb67bc1c05.herokuapp.com/")
+      .then((response) => response.json())
+      .then((data) => {
+        const moviesFromApi = data.docs.map((doc) => {
+          return {
+            id: doc.key,
+            title: doc.title,
+            description: doc.description,
+            genre: {
+              name: doc.genre.name,
+              description: doc.genre.description
+            },
+            director: {
+              name: doc.director.name,
+              bio: doc.bio.name
+            },
+            actors: [doc.actors]
+          };
+        });
+
+        setMovies(moviesFromApi);
+      });
+  }, []); 
+
+  
 
   if (selectedMovie) {
     return (
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,11 +1,30 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
-    return (
-      <div
-        onClick={() => {
-          onMovieClick(movie);
-        }}
-      >
-        {movie.title}
-      </div>
-    );
-  };
+  return (
+    <div
+      onClick={() => {
+        onMovieClick(movie);
+      }}
+    >
+      {movie.title}
+    </div>
+  );
+};
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired
+    }),
+    director: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      bio: PropTypes.string
+    }),
+    actors: PropTypes.string
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
