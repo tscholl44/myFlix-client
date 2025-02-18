1eaf97dfda5b56fb4682c800b5d899fd01b2c295
tscholl44 <tscholl4444@gmail.com>
2025-02-18 16:58:32 -0500

    create login-view
    react forms for user login

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+
+export const LoginView = ({ onLoggedIn }) => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+    event.preventDefault();
+
+    const data = {
+      access: name,
+      secret: password
+    };
+
+    fetch("https://toms-flix-a1bb67bc1c05.herokuapp.com/login", {
+      method: "POST",
+      body: JSON.stringify(data)
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Login response: ", data);
+        if (data.user) {
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", data.token);  
+          onLoggedIn(data.user, data.token);
+        } else {
+          alert("No such user");
+        }
+      })
+      .catch((e) => {
+        alert("Something went wrong");
+      });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
