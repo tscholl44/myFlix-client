577ca97e47d221787b5f4b6fb3342302ab465899
tscholl44 <tscholl4444@gmail.com>
2025-02-18 16:59:23 -0500

    Create signup-view.jsx
    react forms for user signup
    SIGNUP_URL tbd
    check for letter case agreement with movie-api schema

---
+++ src/components/signup-view/signup-view.jsx
@@ -0,0 +1,77 @@
+import{ useState } from "react";
+
+export const SignupView = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: name,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    };
+
+    fetch("SIGNUP_URL", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then((response) => {
+      if (response.ok) {
+        alert("Signup successful");
+        window.location.reload();
+      } else {
+        alert("Signup failed");
+      }
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        name:
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          minLength="3"
+        />
+      </label>
+      <label>
+        password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        birthday:
+        <input
+          type="date"
+          value={birthday}
+          onChange={(e) => setBirthday(e.target.value)}
+          required
+        />
+      </label>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
