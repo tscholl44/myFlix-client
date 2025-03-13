f47661ea14fdae53bfe66edf93bb83c64b465887
tscholl44 <tscholl4444@gmail.com>
2025-03-13 18:21:15 -0400

    add style to back button

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,3 +1,5 @@
+import "./movie-view.scss";
+
 export const MovieView = ({ movie, onBackClick }) => {
     return (
       <div>
@@ -12,7 +14,13 @@ export const MovieView = ({ movie, onBackClick }) => {
           <span>Director: </span>
           <span>{movie.director}</span>
         </div>
-        <button onClick={onBackClick}>Back</button>
+        <button 
+          onClick={onBackClick} 
+          className="back-button"
+          style={{ cursor: "pointer" }}
+        >
+          Back
+        </button>
       </div>
     );
   };
