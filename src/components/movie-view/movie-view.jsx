d61bd326a8586623f4c980ae2fc785b9e8872874
tscholl44 <tscholl4444@gmail.com>
2025-03-25 17:04:24 -0400

    add placeholder image and conditional for not found

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,26 +1,26 @@
 import "./movie-view.scss";
 
 export const MovieView = ({ movie, onBackClick }) => {
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <img src={movie.image} />
-        </div>
-        <div>
-          <span>Title: </span>
-          <span>{movie.title}</span>
-        </div>
-        <div>
-          <span>Director: </span>
-          <span>{movie.director}</span>
-        </div>
-        <button 
-          onClick={onBackClick} 
-          className="back-button"
-          style={{ cursor: "pointer" }}
-        >
-          Back
-        </button>
+        <img src={movie?.image || "https://via.placeholder.com/150"} alt={movie?.title || "Placeholder"} />
       </div>
-    );
-  };
+      <div>
+        <span>Title: </span>
+        <span>{movie?.title || "N/A"}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie?.director || "N/A"}</span>
+      </div>
+      <button 
+        onClick={onBackClick} 
+        className="back-button"
+        style={{ cursor: "pointer" }}
+      >
+        Back
+      </button>
+    </div>
+  );
+};
