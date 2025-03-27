c72b25b1e83a490676b410cff9a1aae81798f2bd
tscholl44 <tscholl4444@gmail.com>
2025-03-27 18:10:52 -0400

    Update movie-view.jsx
    include all movie data fields from API JSON doc

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,26 +1,61 @@
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+
+import { MovieCard } from '../movie-card/movie-card';
 import "./movie-view.scss";
 
-export const MovieView = ({ movie, onBackClick }) => {
+export const MovieView = ({ movies }) => {
+  const { movieId } = useParams();
+
+  const movie = movies.find((m) => m.id === movieId);
+
+  if (!movie) {
+    return (
+      <div>
+        <p>Movie not found.</p>
+        <Link to={`/`}>
+          <button className="back-button">Back</button>
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <div>
       <div>
-        <img src={movie?.image || "https://via.placeholder.com/150"} alt={movie?.title || "Placeholder"} />
+        <img
+          className="w-100"
+          src={movie.image || "https://via.placeholder.com/300x450?text=No+Image+Available"}
+          alt={movie.title}
+        />
       </div>
       <div>
         <span>Title: </span>
-        <span>{movie?.title || "N/A"}</span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.description}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.genre.name}</span>
+      </div>
+      <div>
+        <span>Genre Description: </span>
+        <span>{movie.genre.description}</span>
       </div>
       <div>
         <span>Director: </span>
-        <span>{movie?.director || "N/A"}</span>
+        <span>{movie.director.name}</span>
       </div>
-      <button 
-        onClick={onBackClick} 
-        className="back-button"
-        style={{ cursor: "pointer" }}
-      >
-        Back
-      </button>
+      <div>
+        <span>Director Bio: </span>
+        <span>{movie.director.bio}</span>
+      </div>
+      <Link to={`/`}>
+        <button className="back-button">Back</button>
+      </Link>
     </div>
   );
 };
