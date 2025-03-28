3c58887d04d72989a736fe0589d06dcbb9de8fad
tscholl44 <tscholl4444@gmail.com>
2025-03-28 17:50:26 -0400

    add handleFavoriteUpdate to MainView so it's no longer undefined in any components

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,11 +4,10 @@ import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
-import {ProfileView} from "../profile-view/profile-view";
+import { ProfileView } from "../profile-view/profile-view";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -16,16 +15,14 @@ export const MainView = () => {
   const [movies, setMovies] = useState([]);
   const [user, setUser] = useState(storedUser);
   const [token, setToken] = useState(storedToken);
-  
-  
-  useEffect(() => {
 
+  useEffect(() => {
     if (!token) {
       return;
     }
 
     fetch("https://toms-flix-a1bb67bc1c05.herokuapp.com/movies", {
-      headers: { Authorization: `Bearer ${token}` }
+      headers: { Authorization: `Bearer ${token}` },
     })
       .then((response) => {
         if (!response.ok) {
@@ -34,7 +31,6 @@ export const MainView = () => {
         return response.json();
       })
       .then((data) => {
-        console.log("API Response:", data); 
         const moviesFromApi = data.map((movie) => ({
           id: movie._id.toString(),
           _id: movie._id,
@@ -42,11 +38,11 @@ export const MainView = () => {
           description: movie.description,
           genre: {
             name: movie.genre.name,
-            description: movie.genre.description
+            description: movie.genre.description,
           },
           director: {
             name: movie.director.name,
-            bio: movie.director.bio
+            bio: movie.director.bio,
           },
         }));
         setMovies(moviesFromApi);
@@ -54,7 +50,43 @@ export const MainView = () => {
       .catch((error) => {
         console.error("Error fetching movies:", error);
       });
-  }, [token]); 
+  }, [token]);
+
+  // Define handleFavoriteUpdate
+  const handleFavoriteUpdate = (movieId, action) => {
+    const updatedFavorites =
+      action === "add"
+        ? [...user.favoriteMovies, movieId]
+        : user.favoriteMovies.filter((id) => id !== movieId);
+
+    fetch(
+      `https://toms-flix-a1bb67bc1c05.herokuapp.com/users/${user.name}/favoriteMovies/${movieId}`,
+      {
+        method: action === "add" ? "POST" : "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to update favorite movies");
+        }
+        return response.json();
+      })
+      .then((updatedUserData) => {
+        setUser(updatedUserData);
+        setMovies((prevMovies) =>
+          prevMovies.map((movie) =>
+            updatedFavorites.includes(movie._id)
+              ? { ...movie, isFavorite: true }
+              : { ...movie, isFavorite: false }
+          )
+        );
+      })
+      .catch((error) => console.error("Error updating favorite movies:", error));
+  };
 
   return (
     <BrowserRouter>
@@ -124,7 +156,6 @@ export const MainView = () => {
               </>
             }
           />
-
           <Route
             path="/"
             element={
@@ -137,7 +168,12 @@ export const MainView = () => {
                   <>
                     {movies.map((movie) => (
                       <Col className="mb-4" key={movie.id} md={3}>
-                        <MovieCard movie={movie} user={user} setUser={setUser} />
+                        <MovieCard
+                          movie={movie}
+                          user={user}
+                          setUser={setUser}
+                          handleFavoriteUpdate={handleFavoriteUpdate} // Pass the function here
+                        />
                       </Col>
                     ))}
                   </>
